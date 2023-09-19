c09c6c6c5ff87a7b9109fc1e5b8390202d646922
MrinskiyAndrey <nomarg@yandex.ru>
2023-09-19 09:49:36 +0300

    Added original code

--- TPP_Homework/TPP_Homework.cpp
+++ TPP_Homework/TPP_Homework.cpp
@@ -1,29 +1,30 @@
-﻿#include"UnitTests.h"
+﻿#include<iostream>
+#include<iomanip>
+#include<ctime>
+#include<math.h>
+#include<cassert>
 
-    double z(double x1, double x2) {
-        return pow(x1, 2) + 3 * pow(x2, 2);
-    }
-    double g1(double x1, double x2) {    
-        return -x2 + 3 * pow(x1, 2) + 2;
-    }
-    double g2(double x1, double x2) {
-        return x2 + x1 - 1;
-    }
-    double sum(double x1, double x2) {
-        return x1 + x2;
-    }   
+using namespace std;
+
+double z(double x1, double x2) {
+    return pow(x1, 2) + 3 * pow(x2, 2);
+}
+double g1(double x1, double x2) {
+    return -x2 - 3 * pow(x1, 2) + 2;
+}
+double g2(double x1, double x2) {
+    return x2 + x1 - 1;
+}
+double sum(double x1, double x2) {
+    return x1 + x2;
+}
 
 int main()
 {
     setlocale(LC_ALL, "Rus");
     srand(time(NULL));
 
-    Test_z();
-    Test_g1();
-    Test_g2();
-    Test_sum();
-
-    int N, m = 0, m1 = 0, r = 0, r1 = 0;
+    int N, m = 0, m1 = 0, r, r1;
     double x1a, x1b, x2a, x2b, minz, minz1, e, d, alfa;
 
     cout << "N=";
@@ -67,16 +68,16 @@ int main()
     for (int i = 0; i < N; i++) {
         x1_1[i] = (((x1[r] + e) - (x1[r] - e)) * ((double)rand() / RAND_MAX)) + (x1[r] - e);
         x2_1[i] = (((x2[r] + e) - (x2[r] - e)) * ((double)rand() / RAND_MAX)) + (x2[r] - e);
-        if (g1(x1_1[i], x2_1[i]) >= 0 and g2(x1_1[i], x2_1[i]) >= 0) { 
+        if (g1(x1_1[i], x2_1[i]) >= 0 and g2(x1_1[i], x2_1[i])) {
             Z1[i] = z(x1_1[i], x2_1[i]);
-            if (Z[r] - Z1[i] <= alfa) { 
+            if (sqrt(pow(Z[r] - Z1[i], 2)) <= alfa) {
                 minz1 = Z1[i];
                 r1 = i;
                 cout << "Условие по alfa тоже выполнено" << endl;
-                break;
+                goto metka;
             }
             if (m1 < 1) {
-                minz1 = Z1[i];     
+                minz1 = Z1[i];
                 m1 = m + 1;
             }
             if (minz1 > Z1[i]) {
@@ -85,10 +86,10 @@ int main()
             }
         }
     }
-
-    cout << "minz=" << minz << " x1=" << x1[r] << " x2=" << x2_1[r1] << endl;
-    cout << "minz1=" << minz1 << " x1_1=" << x1_1[r1] << " x2_1=" << x2_1[r1] << endl;
-    cout << "minz-minz1=" << minz - minz1 << endl;
+metka:
+    cout << "minz=" << minz << " " << "x1=" << x1[r] << " " << "x2=" << x2_1[r1] << endl;
+    cout << "minz1=" << minz1 << " " << "x1_1=" << x1_1[r1] << " " << "x2_1=" << x2_1[r1] << endl;
+    cout << "minz-minz1=" << sqrt(pow(Z[r] - Z[r1], 2)) << endl;
 
     system("pause");
     return 0;
